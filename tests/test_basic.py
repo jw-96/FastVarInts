4aeb90a9bbaffd999b2227f5eb37aeb76a5b3cf2
Jeffrey Wigger <jeffrey.wigger@epfl.ch>
2022-05-23 15:08:31 +0200

    added tests

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,13 +1,15 @@
-import scikit_build_example as m
-
+import fastVarInts as fvi
+import numpy as np
 
 def test_version():
-    assert m.__version__ == "0.0.1"
+    assert fvi.__version__ == "0.0.1"
 
 
-def test_add():
-    assert m.add(1, 2) == 3
+def test_comp_decomp():
+    arr = np.random.poisson(30, 5000000) + 1 # elias does not work on 0s # .frame.decompress
+    arr= arr.astype(np.uint32)
+    comp = fvi.compress(arr)
+    arr_dec = fvi.decompress(comp)
+    assert len(arr) == len(arr_dec)
+    assert (arr == arr_dec).all()
 
-
-def test_sub():
-    assert m.subtract(1, 2) == -1
